defc579514bd8202a3e45fae3b9988f59d9af849
Nicolai Larsen <nla@sitemule.com>
2020-03-05 16:28:54 +0100

    magic

--- battle/server-core/socket.js
+++ battle/server-core/socket.js
@@ -10,18 +10,25 @@ exports.initSocket = () => {
     io = require('socket.io')(http);
     io.on('connection', (socket) => {
         _clients.push(socket);
-        console.log('a user connected');
-
-        
+        console.log('a user connected');     
+        console.log(_clients)
         socket.on('attack', typeOfAttack => {
             console.log(typeOfAttack)
             io.emit('attack', typeOfAttack)
         })
+        socket.on('getClients', clientId => {
+            if (clientId) {
+                socket.emit(this.getClient(clientId))
+            } else {
+                socket.emit(this.getClients())
+            }
+        })
     });
+
+
     http.listen(3000, function () {
         console.log('listening on *:3000');
     });
-
 }
 exports.broadcast = (event, param) => io.emit(event,param);
 exports.emitToClient = (clientId, event, param) => this.getClient(client).emit(event, param);
